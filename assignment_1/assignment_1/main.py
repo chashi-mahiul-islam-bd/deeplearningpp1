a8ebf0ca9a087884b74031b0352e2591926183bc
venkat <vb19h@my.fsu.edu>
2021-02-24 22:44:29 -0500

    adding the Fully Connected model to repository

--- assignment_1/assignment_1/main.py
+++ assignment_1/assignment_1/main.py
@@ -1,11 +1,17 @@
 import toml
+from trainer import dataset_creation, train
 
 
 def process(conf):
     """
-
-    :param conf: Configurator
-    :return:
+    
+    :param conf: Configurator 
+    :return: 
     """
+    conf = dataset_creation(conf)
+    train(conf)
 
+if __name__=="__main__":
+    conf = toml.load("../assignment_1/assignment_1/mainconfig.toml")
+    process(conf)
 
